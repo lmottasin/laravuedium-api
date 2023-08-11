38c040127f476e118ec617c96ab4595e70eb2b09
lmottasin <lmottasin@gmail.com>
2023-08-11 17:24:29 +0600

    feat: login, logout api implement

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\API\V1\Auth\LogoutController;
 use App\Http\Controllers\API\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'auth:sanctum'], function (){
+    Route::post('auth/logout', LogoutController::class);
+
+    Route::get('user', function (Request $request){
+        return $request->user();
+    });
+});
 
 Route::post('auth/register', RegisterController::class);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('auth/login', LoginController::class);
