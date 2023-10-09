a0421785f12610b84485f59f80de472905639d42
lmottasin <lmottasin@gmail.com>
2023-10-09 22:01:24 +0600

    feat: github login

--- app/Http/Controllers/API/V1/Auth/SocialLoginController.php
+++ app/Http/Controllers/API/V1/Auth/SocialLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use ErrorException;
 use Exception;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\Request;
@@ -14,60 +15,62 @@ use Laravel\Socialite\Two\GithubProvider;
 
 class SocialLoginController extends Controller
 {
+    /**
+     * @throws RequestException
+     * @throws ErrorException
+     * @throws Exception
+     */
     public function handleProviderCallback($provider, Request $request)
     {
-        try {
-            $data = Http::acceptJson()
-                ->post('https://github.com/login/oauth/access_token', [
-                    'client_id' => config('services.github.client_id'),
-                    'client_secret' => config('services.github.client_secret'),
-                    'code' => $request->get('code'),
-                    'redirect_uri' => config('services.github.redirect'),
-                ])
-                ->throw()
-                ->json();
 
-            $socialUser = Socialite::driver($provider)->stateless()->userFromToken($data['access_token']);
+        $data = Http::acceptJson()
+            ->post('https://github.com/login/oauth/access_token', [
+                'client_id' => config('services.github.client_id'),
+                'client_secret' => config('services.github.client_secret'),
+                'code' => $request->get('code'),
+                'redirect_uri' => config('services.github.redirect'),
+            ])
+            ->throw()
+            ->json();
 
-            /*$publicEmailsUrl = 'https://api.github.com/user/public_emails';
-
-            try {
-                $response = Http::acceptJson()->withToken($data['access_token'])->get(
-                    $publicEmailsUrl
-                )->json();
-
-                foreach ($response as $email) {
-                    if ($email['primary'] && $email['verified']) {
-                        return $email['email'];
-                    }
-                }
-            } catch (Exception $e) {
-                return $e->getMessage();
-            }*/
-
-            return response()->json($socialUser);
-
-            $user = $this->oauthLogin($socialUser);
-
-            $user['token'] = $user->createToken('main')->plainTextToken;
-
-            return response()->json($user);
-
-        } catch (Exception $exception) {
-            return response()->json($exception);
+        if (isset($data['error'])) {
+            return throw new ErrorException($data['error_description']);
         }
+
+
+        $socialUser = Socialite::driver($provider)->stateless()->userFromToken($data['access_token']);
+
+        if (is_null($socialUser['email'])) {
+            $publicEmailsUrl = 'https://api.github.com/user/public_emails';
+            $response = Http::acceptJson()->withToken($data['access_token'])->get($publicEmailsUrl)->throw()->json();
+
+            foreach ($response as $email) {
+                if ($email['primary'] && $email['verified']) {
+                    $socialUser['email'] = $email['email'];
+                    break;
+                }
+            }
+        }
+
+        $user = $this->oauthLogin($socialUser['name'], $socialUser['email']);
+
+        $user['token'] = $user->createToken(now().random_int(1, 100))->plainTextToken;
+
+        return response()->json($user);
+
     }
 
-    public function oauthLogin($user)
+
+    public function oauthLogin(string $name, string $email)
     {
         $user = User::firstOrCreate(
             [
-                'name' => $user->name ?: $user->nickname,
-                'email' => $user->email,
+                'name' => $name,
+                'email' => $email,
             ],
             [
-                'name' => $user->name ?: $user->nickname,
-                'email' => $user->email,
+                'name' => $name,
+                'email' => $email,
                 'password' => Hash::make(md5(uniqid().now())),
                 'email_verified_at' => now()
             ]
